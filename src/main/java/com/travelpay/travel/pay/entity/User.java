b9f04c63066c70c937e980520c3233821414a12e
Souvik Mondal <sm6.souvik@gmail.com>
2026-09-06 03:52:09 +0530

    created packages

---
+++ src/main/java/com/travelpay/travel/pay/entity/User.java
@@ -0,0 +1,25 @@
+package com.travelpay.travel.pay.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private String phoneNumber;
+}
